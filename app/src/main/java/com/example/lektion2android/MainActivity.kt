875b3034301235c31829a56a5353fa306134c451
Magnus Ahlqvist <51696488+Magnusahl@users.noreply.github.com>
2020-04-03 14:27:10 +0200

    updated code

--- app/src/main/java/com/example/lektion2android/MainActivity.kt
+++ app/src/main/java/com/example/lektion2android/MainActivity.kt
@@ -1,31 +1,63 @@
 package com.example.lektion2android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+
+val EXTRA_MESSAGE = "com.example.Lektion2Android.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
         lateinit var questionView : TextView
+        var firstNumber = 0
+        var secondNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         questionView = findViewById(R.id.questionText)
+        randomizeQuestion()
 
-        val firstNumber = (1..10).random()
-        val secondNumber = (1..10).random()
+        val button = findViewById<Button>(R.id.button)
 
-        questionView.text = "$firstNumber + $secondNumber ="
-
-
+        button.setOnClickListener {
+            startAnswerActivity()
+        }
 
     }
 
+    override fun onRestart() {
+        super.onRestart()
+
+        randomizeQuestion()
+    }
+
+    fun checkAnswer() : Boolean {
+        val answerText = findViewById<EditText>(R.id.answerText)
+        val answer = answerText.text.toString()
+        val answerInt = answer.toIntOrNull()
+
+        return firstNumber + secondNumber == answerInt
+    }
+
+    fun startAnswerActivity() {
+        val rightAnswer = checkAnswer()
+        intent.putExtra(EXTRA_MESSAGE, rightAnswer)
+        val intent = Intent(this, AnswerActivity::class.java)
+        startActivity(intent)
+    }
 
 
+    fun randomizeQuestion() {
+        firstNumber = (1..10).random()
+        secondNumber = (1..10).random()
+        questionView.text = "$firstNumber + $secondNumber ="
+    }
 
 
 }
